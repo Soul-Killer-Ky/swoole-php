446bf84a3df3cd7057644860f95f81ba09d11ee4
keyi <keyi_4618@163.com>
2018-07-28 11:47:42 +0800

    ws

--- ws_server.php
+++ ws_server.php
@@ -8,8 +8,11 @@
 //创建websocket服务器对象，监听0.0.0.0:9502端口
 $ws = new swoole_websocket_server("0.0.0.0", 9504);
 
+$fd = [];
+
 //监听WebSocket连接打开事件
-$ws->on('open', function ($ws, $request) {
+$ws->on('open', function ($ws, $request) use ($fd) {
+    $fd[] = $request->fd;
     var_dump($request->fd, $request->get, $request->server);
     $ws->push($request->fd, "hello, welcome\n");
 });
@@ -17,6 +20,9 @@ $ws->on('open', function ($ws, $request) {
 //监听WebSocket消息事件
 $ws->on('message', function ($ws, $frame) {
     echo "Message: {$frame->data}\n";
+    swoole_timer_tick(2000, function ($timer_id) use ($ws, $frame) {
+        $ws->push($frame->fd, "timer: " . $frame->data);
+    });
     $ws->push($frame->fd, "server: {$frame->data}");
 });
 
